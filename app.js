84ffd320f621c73b0d395f2a7f7941b792f588cf
BridulMunoth <bridulmunoth2024@gmail.com>
2026-02-13 12:20:13 +0530

    Version update

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Live Project - Version 1 🚀');
+  res.send('DevOps Live Project - Version 2 🚀');
 });
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
+
